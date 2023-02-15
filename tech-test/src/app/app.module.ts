8adce8472dd1808e7f63502926d967302ba92443
Dave Rojas <info@daverojas.net>
2023-02-15 10:58:40 +0100

    Initial scafolfing components

--- tech-test/src/app/app.module.ts
+++ tech-test/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
